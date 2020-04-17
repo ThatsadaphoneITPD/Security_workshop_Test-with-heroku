79cafeb8be7efe8e99f1e88e89c0c90e16fc333d
ThatsadaphoneITPD <59547857+ThatsadaphoneITPD@users.noreply.github.com>
2020-04-17 22:44:26 +0700

    Update me.php

--- me.php
+++ me.php
@@ -1,9 +1,8 @@
-  
-<html>
-<head>
-<title>PHP Test</title>
-</head>
-<boby>
-<?php echo '<p>Hello World</p>'; ?> 
-</boby>
+  <html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
 </html>
